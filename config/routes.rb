e48b684f5961b7a3226b5c5724dff93dcb9e1931
Stephan Yu <Stephan.yu@gmail.com>
2017-04-01 17:16:19 +0100

    Create pages controller via generator for home about contact page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/about'
+
+  get 'pages/contact'
+
   get 'hello_world', to: 'hello_world#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
